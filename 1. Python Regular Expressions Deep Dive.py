06ba108810308d66a66a3d4176c9b775acb40947
rpeloso21 <rpeloso21@gmail.com>
2024-06-29 23:45:17 -0400

    third commit

--- 1. Python Regular Expressions Deep Dive.py
+++ 1. Python Regular Expressions Deep Dive.py
@@ -1,9 +1,10 @@
-# Task 1
 
 import re
 
 text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
 emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
+
+# Task 1
 
 for email in emails:
     email_match = re.search(r"@exclude.com", email)
